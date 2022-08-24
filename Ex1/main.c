f5a5643107aafb8aa525eed0f24475cb3c147abb
Matheus Negrini <55109057+madrigueira@users.noreply.github.com>
2022-08-24 20:13:26 -0300

    Chupa Pedro

---
+++ Ex1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float n1 = 0;
+	float n2 = 0;
+	float m = 0;
+	
+	printf("Primeira Nota:");
+	scanf("%f", &n1);
+	printf("Segunda Nota:");
+	scanf("%f", &n2);
+	
+	m=(n1+n2)/2;
+	
+	printf("Sua media e: %f",m);
+	
+	return 0;
+}
