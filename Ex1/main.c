f28bfdc1b6c4f894ef3b57a9831c7cefe70ddedc
Matheus Negrini <55109057+madrigueira@users.noreply.github.com>
2022-08-24 20:23:25 -0300

    ex1

--- Ex1/main.c
+++ Ex1/main.c
@@ -7,14 +7,17 @@ int main(int argc, char *argv[]) {
 	
 	float n1 = 0;
 	float n2 = 0;
+	float n3 = 0;
 	float m = 0;
 	
 	printf("Primeira Nota:");
 	scanf("%f", &n1);
 	printf("Segunda Nota:");
 	scanf("%f", &n2);
+	printf("Terceira Nota:");
+	scanf("%f", &n3);
 	
-	m=(n1+n2)/2;
+	m=(n1+n2+n3)/3;
 	
 	printf("Sua media e: %f",m);
 	
