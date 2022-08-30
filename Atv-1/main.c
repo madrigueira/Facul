71526920c4c5b6c55af535ff5e4112f83024b294
Matheus Negrini <55109057+madrigueira@users.noreply.github.com>
2022-08-30 00:33:05 -0300

    tava errado

--- Atv-1/main.c
+++ Atv-1/main.c
@@ -2,20 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	
-	float D = 0, L = 0, K = 0, N = 0, C = 0;
-	
-	printf("		===> CUSTO VIAGEM <=== \n\n   Vamos descobrir quantos litros de gasolina sao necessarios para\n   realizar o seu trajeto e quanto voce tera que desenbolsar pra isso!!\n\n\n");
-	printf("   - Digite a distancia percorrida em Km: ");
-	scanf("%f", &D);
-	printf("\n   - Digite o preco do litro da gasolina: ");
-	scanf("%f", &L);
-	printf("\n   - Digite a quilometragem que o veiculo faz por litro: ");
-	scanf("%f", &K);
-	#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
     
     float D = 0, L = 0, K = 0, N = 0, C = 0;
     
